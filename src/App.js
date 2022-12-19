99c3ac2f6915d87b142165cec0586602d41758a8
Luong Truong Quang <LuongTQ2@fpt.com.vn>
2022-12-19 11:22:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://10.96.34.233:8080/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Jenkins
         </a>
       </header>
     </div>
